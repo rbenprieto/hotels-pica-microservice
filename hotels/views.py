97abb328fcd17bf7d45ecc4a02d411c7d6666e33
rbenprieto <ruben.prieto@fracttal.com>
2024-11-11 22:32:54 +0100

    rooms by hotel

--- hotels/views.py
+++ hotels/views.py
@@ -24,8 +24,12 @@ class RoomsView(generics.ListAPIView):
 
     def get(self, request):
         query_param = request.query_params.get("id")
+        hotel_query_param = request.query_params.get("hotel")
         rooms = self.get_queryset()
         if query_param:
             room = get_object_or_404(Rooms, id=query_param)
             return Response(RoomsSerializer(room).data)
+        if hotel_query_param:
+            rooms = self.get_queryset().filter(hotel=hotel_query_param)
+            return Response(RoomsSerializer(rooms, many=True).data)
         return Response(RoomsSerializer(rooms, many=True).data)
